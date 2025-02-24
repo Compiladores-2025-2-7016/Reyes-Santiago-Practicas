97153966905e2c3db9ffe27f2d67c6f5ab2826b6
santiago861 <santiago_reyes@ciencias.unam.mx>
2025-02-24 16:33:43 -0600

    ultimo ejercicio

--- Practica1/env-practica1/src/segundo_lex/lex.py
+++ Practica1/env-practica1/src/segundo_lex/lex.py
@@ -8,6 +8,7 @@ letra = r'[a-zA-Z]'
 hexadecimal = r'0[xX][0-9a-fA-F]+'
 numero = r'(' + digito + r')+'
 identificador_java = r'[a-zA-Z_][a-zA-Z0-9_]{0,31}'
+espacio_en_blanco = r'[ \t]+'
 
 # Lista de palabras reservadas de Python
 reservadas_python = {
@@ -22,7 +23,8 @@ tokens = (
     "PARDER",
     "HEX",
     "RESERVADA_PYTHON",
-    "ID_JAVA"
+    "ID_JAVA",
+    "ESPACIO"
 )
 
 # Definición de reglas en una sola línea sin acción léxica
@@ -33,11 +35,13 @@ t_PARDER = r'\)'
 # Definición de reglas con acción léxica
 @TOKEN(hexadecimal)
 def t_HEX(t):
+    print('')
     print("Encontré un número hexadecimal:", t.value)
     return t
 
 @TOKEN(numero)
 def t_NUMERO(t):
+    print('')
     print("Encontré un número:", t.value)
     return t
 
@@ -45,9 +49,17 @@ def t_NUMERO(t):
 def t_ID_JAVA(t):
     if t.value in reservadas_python:
         t.type = "RESERVADA_PYTHON"
+        print('')
         print("Palabra reservada de Python detectada:", t.value)
     else:
+        print('')
         print("Identificador válido de Java:", t.value)
+    return t
+
+@TOKEN(espacio_en_blanco)
+def t_ESPACIO(t):
+    print('')
+    print("Espacio en blanco detectado:", repr(t.value))
     return t
 
 # Manejo de saltos de línea
@@ -55,11 +67,12 @@ def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
-# Ignorar espacios en blanco y tabuladores
-t_ignore  = " \t"
+# Ignorar otros caracteres no especificados
+t_ignore = ""
 
 # Manejo de errores léxicos
 def t_error(t):
+    print('')
     print(f"Error léxico en línea {t.lexer.lineno}, columna {t.lexpos}: '{t.value[0]}'")
     t.lexer.skip(1)
 
@@ -76,6 +89,7 @@ def funcion():
         return 42
     _identificadorValidoJava32chars_abcdefghijklmno
     if else
+    
 '''
 
 # En caso de que estemos leyendo un archivo señalado desde la línea de argumentos
